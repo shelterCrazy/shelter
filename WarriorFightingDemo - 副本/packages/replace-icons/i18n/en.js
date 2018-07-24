084d91ebeab6f88eb56fdfcb541270d2eb08e622
C4jiang <1349363557@qq.com>
2018-07-24 16:54:50 +0800

    删除多余文件。。。

--- WarriorFightingDemo - 副本/packages/replace-icons/i18n/en.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    title: 'Replace Icons',
-    projProp: 'Target Project',
-    platforms: 'Platforms',
-    pngPath: '.png File',
-    icnsPath: '.icns File',
-    icoPath: '.ico File',
-    replace: 'Replace'
-};
