c030ac96511e3fa041a835241b0268bad713e111
C4jiang <1349363557@qq.com>
2018-09-03 19:50:11 +0800

    回滚一下进度

--- WarriorFightingDemo/assets/random.js
+++
@@ -1,9 +0,0 @@
-Math.seed = 5;
-Math.seededRandom = function(min, max)
-{
-    max = max || 1;
-    min = min || 0;
-    Math.seed = (Math.seed * 9301 + 49297) % 233280;
-    var rnd = Math.seed / 233280.0;
-    return min + rnd * (max - min);
-};
